8d161ff1aae70feb8d60b8a8fb5d3ffcda39a5c2
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2023-04-10 15:37:41 +0300

    no message

--- app/src/main/java/com/example/sinyord/login.java
+++ app/src/main/java/com/example/sinyord/login.java
@@ -1,40 +1,30 @@
 package com.example.sinyord;
-import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Button;
-import androidx.appcompat.app.AppCompatActivity;
-
-
+import android.widget.EditText;
+import android.widget.Toast;
 public class login extends AppCompatActivity {
-    Button b1;
-    EditText ed1,ed2;
-
+    EditText txtkullanıcıadi;
+    EditText txtsifre;
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-    }
-
-    b1=(Button) findViewById(R.id.button);
-    ed1=(EditText) findViewById(R.id.editText);
-    ed2=(EditText)findViewById(R.id.editText2);
-    b1.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            if(ed1.getText().toString().equals("Fikret")&&
-                    ed2.getText().toString().equals("123456")){
-                Intent i = new Intent(login.this, SecondaryActivity.class);
-                i.putExtra("id",ed1.getText().toString());
-                startActivity(i);
+        txtkullanıcıadi = findViewById(R.id.kullaniciadi);
+        txtsifre = findViewById(R.id.sifre);
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (txtkullanıcıadi.getText().toString().equals("user") && txtsifre.getText().toString().equals("1234")) {
+                    Toast.makeText(login.this, "Giriş Başarılı!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(login.this, "Giriş Başarısız!", Toast.LENGTH_SHORT).show();
+                }
             }
-        }
-    });
-
-
-//<!-- https://ronurdemirtas.wordpress.com/2015/08/04/androidde-login-form-nasil-yapilir/ -->
-
-
-
+        });
+    }
 }
